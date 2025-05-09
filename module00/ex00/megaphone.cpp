3cfbfbd3dc235d70627a0b03b9c8c21e52f04b81
Yutosaki <sasayu8190@gmail.com>
2025-05-09 17:30:14 +0900

    feat: add prototype

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsasak <yutsasak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 17:25:55 by yutsasak          #+#    #+#             */
+/*   Updated: 2025/05/09 17:25:56 by yutsasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+std::string toUpper(const std::string& str) {
+    std::string result;
+
+    for (size_t i = 0; i < str.length(); i++)
+        result += std::toupper(str[i]);
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    std::string upper;
+
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; i++) {
+        upper += toUpper(argv[i]);
+        if (i != argc - 1)
+            upper += " ";
+    }
+    std::cout << upper << std::endl;
+    return 0;
+}
