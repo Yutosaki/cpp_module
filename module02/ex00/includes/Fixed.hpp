4eac8999ab10403781dfa36eaab5b0940b390892
Yutosaki <sasayu8190@gmail.com>
2025-08-05 15:23:02 +0900

    feat: add ex00

---
+++ module02/ex00/includes/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &other);
+        Fixed &operator=(const Fixed &other);
+
+        int getRawBits() const;
+        void setRawBits(int const raw);
+    private:
+        int _rawBits;
+        static const int _fractionalBits = 8;
+};
+
+#endif
