e2d44e9e8d655bf9668004b5006c2461d9eb2d59
Yutosaki <sasayu8190@gmail.com>
2025-09-07 15:27:39 +0900

    feat: add main test which confirm brain construct and delete order

--- module04/ex01/srcs/main.cpp
+++ module04/ex01/srcs/main.cpp
@@ -1,35 +1,51 @@
 #include "Animal.hpp"
-#include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
+#include "Cat.hpp"
+#include "Brain.hpp"
 
 int main() {
-  {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();  // will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+    {
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
 
-    delete meta;
-    delete j;
-    delete i;
-  }
-    std::cout << "---------------------------------" << std::endl;
-  {
-    const WrongAnimal* meta = new WrongAnimal();
-    const WrongAnimal* i = new WrongCat();
-    std::cout << i->getType() << " " << std::endl;
+        delete j;
+        delete i;
+    }
 
-    i->makeSound();
-    meta->makeSound();
+    std::cout << "\n-----------------------------------------\n" << std::endl;
 
-    delete meta;
-    delete i;
-  }
-  return 0;
+    {
+        std::cout << "1. Creating Original Dog on the stack." << std::endl;
+        Dog original_dog;
+
+        {
+            std::cout << "\n2. Creating Copied Dog in a new scope." << std::endl;
+            Dog copied_dog(original_dog);
+            std::cout << "3. Copied Dog is about to go out of scope." << std::endl;
+        }
+
+        std::cout << "\n4. Original Dog is still alive." << std::endl;
+        std::cout << "5. Original Dog is about to go out of scope." << std::endl;
+    }
+
+    std::cout << "\n-----------------------------------------\n" << std::endl;
+
+    {
+        const int numAnimals = 6;
+        Animal* animals[numAnimals];
+
+        for (int i = 0; i < numAnimals / 2; ++i) {
+            animals[i] = new Dog();
+        }
+        for (int i = numAnimals / 2; i < numAnimals; ++i) {
+            animals[i] = new Cat();
+        }
+
+        std::cout << "\n--- Deleting all animals in the array ---\n" << std::endl;
+        for (int i = 0; i < numAnimals; ++i) {
+            delete animals[i];
+        }
+    }
+
+    return 0;
 }
