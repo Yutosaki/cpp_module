79e1f73a45e391390cdee541a2d32d2e7fb8a137
Yutosaki <sasayu8190@gmail.com>
2025-09-07 15:29:14 +0900

    feat: add previous files which is same

---
+++ module04/ex02/srcs/WrongCat.cpp
@@ -0,0 +1,27 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() {
+    type = "WrongCat";
+    std::cout << "WrongCat default constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other) {
+    std::cout << "WrongCat copy constructor called" << std::endl;
+    *this = other;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other) {
+    std::cout << "WrongCat assignment operator called" << std::endl;
+    if (this != &other) {
+        type = other.type;
+    }
+    return *this;
+}
+
+WrongCat::~WrongCat() {
+    std::cout << "WrongCat destructor called" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+    std::cout << "WrongCat sound" << std::endl;
+}
