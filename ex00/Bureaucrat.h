afd7a304f441b93e6b2538ecb1a3b69cb909bcfc
Yutosaki <sasayu8190@gmail.com>
2025-12-11 17:29:21 +0900

    feat: headerの追加

--- ex00/Bureaucrat.h
+++ ex00/Bureaucrat.h
@@ -11,6 +11,27 @@ public:
   ~Bureaucrat();
 
 public:
+  Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+
+  const std::string getGrade() const;
+
+  class GradeTooHighException : public std::exception {
+    virtual const char *what() const throw();
+  };
+  class GradeTooLowException : public std::exeption {
+    virtual const char *what() const throw();
+  };
+
+  void IncrementGrade();
+  void DecrementGrade();
+  static const int kMaxGrade = 1;
+  static const int kMinGrade = 150;
+
+private:
+  const std::string name_;
+  int grade_;
 };
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
 
 #endif
