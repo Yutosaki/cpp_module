2ca30de7e4cf58e41506348d9cdccf2198857559
Yutosaki <sasayu8190@gmail.com>
2025-12-11 19:44:26 +0900

    fix: to be camel case of global variables

--- module05/ex00/Bureaucrat.h
+++ module05/ex00/Bureaucrat.h
@@ -4,32 +4,31 @@
 #include <iostream>
 
 class Bureaucrat {
-public:
-  Bureaucrat();
-  Bureaucrat(const Bureaucrat &Bureaucrat);
-  Bureaucrat(const std::string &name, int grade);
-  ~Bureaucrat();
+   public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat &Bureaucrat);
+    Bureaucrat(const std::string &name, int grade);
+    ~Bureaucrat();
 
-public:
-  Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+    Bureaucrat &operator=(const Bureaucrat &bureaucrat);
 
-  const std::string getGrade() const;
+    const std::string getGrade() const;
 
-  class GradeTooHighException : public std::exception {
-    virtual const char *what() const throw();
-  };
-  class GradeTooLowException : public std::exeption {
-    virtual const char *what() const throw();
-  };
+    class GradeTooHighException : public std::exception {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+        virtual const char *what() const throw();
+    };
 
-  void IncrementGrade();
-  void DecrementGrade();
-  static const int kMaxGrade = 1;
-  static const int kMinGrade = 150;
+    void IncrementGrade();
+    void DecrementGrade();
+    static const int MAX_GRADE = 1;
+    static const int MIN_GRADE = 150;
 
-private:
-  const std::string name_;
-  int grade_;
+   private:
+    const std::string name_;
+    int grade_;
 };
 
 std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
