a5ee9bc39ef0a6300aafce13a5401a467d7abf67
Yutosaki <sasayu8190@gmail.com>
2025-12-12 22:56:04 +0900

    feat: add ex00 files

---
+++ module05/ex01/include/Bureaucrat.h
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <exception>
+#include <string>
+
+class Bureaucrat {
+   public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat &Bureaucrat);
+    Bureaucrat(const std::string &name, int grade);
+    ~Bureaucrat();
+
+    Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+
+    const std::string &getName() const;
+    int getGrade() const;
+
+    class GradeTooHighException : public std::exception {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+        virtual const char *what() const throw();
+    };
+
+    void IncrementGrade();
+    void DecrementGrade();
+    static const int MAX_GRADE = 1;
+    static const int MIN_GRADE = 150;
+
+   private:
+    const std::string name_;
+    int grade_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ module05/ex01/src/main.cpp
@@ -0,0 +1,46 @@
+#include <exception>
+#include <iostream>
+#include <string>
+
+#include "Bureaucrat.h"
+
+int main(void) {
+    {
+        std::cout << "-------------------OK-------------------\n";
+        Bureaucrat a = Bureaucrat("a", 2);
+        try {
+            std::cout << a << "\n";
+            a.DecrementGrade();
+            std::cout << a << "\n";
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << "\n";
+            a.IncrementGrade();
+        }
+        std::cout << '\n';
+        {
+            std::cout
+                << "-------------------Grade is too low test-------------------\n";
+            Bureaucrat a = Bureaucrat("a", Bureaucrat::MIN_GRADE);
+            try {
+                std::cout << a << "\n";
+                a.DecrementGrade();
+                std::cout << a << "\n";
+            } catch (const std::exception &e) {
+                std::cerr << e.what() << "\n";
+            }
+        }
+
+        std::cout << '\n';
+        {
+            std::cout
+                << "-------------------Grade is too high test-------------------\n";
+            Bureaucrat b = Bureaucrat("b", Bureaucrat::MAX_GRADE);
+            try {
+                std::cout << b << "\n";
+                b.IncrementGrade();
+            } catch (const std::exception &e) {
+                std::cerr << e.what() << "\n";
+            }
+        }
+    }
+}
