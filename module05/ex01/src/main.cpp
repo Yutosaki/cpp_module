dcf6125e6f653f1193dc2853da09c0bc32c75da4
Yutosaki <sasayu8190@gmail.com>
2025-12-12 23:48:50 +0900

    fix: endl to \n coz linter said it

--- module05/ex01/src/main.cpp
+++ module05/ex01/src/main.cpp
@@ -1,80 +1,83 @@
 #include "Bureaucrat.h"
 #include "Form.h"
 
+#include <iostream>
+#include <exception>
+
 int main(void) {
-    std::cout << "----------------form << operator----------------" << std::endl;
+    std::cout << "----------------form << operator----------------" << "\n";
     try {
         Form a = Form("a", 1, 1);
-        std::cout << a << std::endl;
+        std::cout << a << "\n";
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
-    std::cout << "----------------test too high----------------" << std::endl;
+    std::cout << "----------------test too high----------------" << "\n";
     try {
         Form a = Form("a", 0, 1);
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
     try {
         Form a = Form("a", 1, 0);
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
-    std::cout << "----------------test too low----------------" << std::endl;
+    std::cout << "----------------test too low----------------" << "\n";
     try {
         Form a = Form("a", 151, 1);
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
     try {
         Form a = Form("a", 1, 151);
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
-    std::cout << "----------------test beSigned----------------" << std::endl;
-    std::cout << "----------------OK----------------" << std::endl;
+    std::cout << "----------------test beSigned----------------" << "\n";
+    std::cout << "----------------OK----------------" << "\n";
     try {
         Bureaucrat a = Bureaucrat("a", 1);
         Form b = Form("b", 1, 1);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
         b.beSigned(a);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
-    std::cout << "----------------NG----------------" << std::endl;
+    std::cout << "----------------NG----------------" << "\n";
     try {
         Bureaucrat a = Bureaucrat("a", 10);
         Form b = Form("b", 1, 1);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
         b.beSigned(a);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
     }
 
-    std::cout << "----------------test signForm----------------" << std::endl;
-    std::cout << "----------------OK----------------" << std::endl;
+    std::cout << "----------------test signForm----------------" << "\n";
+    std::cout << "----------------OK----------------" << "\n";
     {
         Bureaucrat a = Bureaucrat("a", 1);
         Form b = Form("b", 10, 1);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
         a.signForm(b);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
     }
 
-    std::cout << "----------------NG----------------" << std::endl;
+    std::cout << "----------------NG----------------" << "\n";
     {
         Bureaucrat a = Bureaucrat("a", 10);
         Form b = Form("b", 1, 1);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
         a.signForm(b);
-        std::cout << b << std::endl;
+        std::cout << b << "\n";
     }
 }
