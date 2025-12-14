417f2c3df64b9ba28ade589f2af22e3d3908dfc0
Yutosaki <sasayu8190@gmail.com>
2025-12-14 21:20:53 +0900

    feat: add edited bureaucrat

---
+++ ex02/include/Bureaucrat.h
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <exception>
+#include <string>
+
+#include "AForm.h"
+
+class AForm;
+
+class Bureaucrat {
+   public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat &Bureaucrat);
+    Bureaucrat(const std::string &name, int grade);
+    ~Bureaucrat();
+
+    Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+
+    const std::string &getName() const;
+    int getGrade() const;
+
+    class GradeTooHighException : public std::exception {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+        virtual const char *what() const throw();
+    };
+
+    void IncrementGrade();
+    void DecrementGrade();
+    static const int MAX_GRADE = 1;
+    static const int MIN_GRADE = 150;
+
+    void signAForm(AForm &form) const;
+    void executeAForm(const AForm &form) const;
+
+   private:
+    const std::string name_;
+    int grade_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
