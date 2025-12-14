142e4424f57680bc12171cb65188939964362dde
Yutosaki <sasayu8190@gmail.com>
2025-12-14 21:48:42 +0900

    feat: add functions

---
+++ module05/ex02/src/PresidentialPardonForm.cpp
@@ -0,0 +1,22 @@
+#include "../include/PresidentialPardonForm.h"
+
+#include "../include/Bureaucrat.h"
+
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardonForm", DEFAULT_GRADE_SIGN, DEFAULT_GRADE_EXEC), target_("default") {}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) : AForm("PresidentialPardonForm", DEFAULT_GRADE_SIGN, DEFAULT_GRADE_EXEC), target_(target) {}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &form) : AForm(form), target_(form.target_) {}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &form) {
+    AForm::operator=(form);
+    return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+const std::string &PresidentialPardonForm::getTarget() const { return target_; };
+
+void PresidentialPardonForm::doExecute() const {
+    std::cout << target_ << " has been pardoned by Zaphod Beeblebrox." << "\n";
+}
