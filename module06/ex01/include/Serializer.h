c7d84469757cb7587c621dd5a227b7cb2de4e96d
Yutosaki <sasayu8190@gmail.com>
2026-01-30 15:30:02 +0000

    feat: add test and functions

---
+++ module06/ex01/include/Serializer.h
@@ -0,0 +1,21 @@
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "Data.h"
+
+class Serializer {
+   public:
+    static uintptr_t serialize(Data* ptr);
+    static Data* deserialize(uintptr_t raw);
+
+   private:
+    Serializer();
+    Serializer(const Serializer& other);
+    Serializer& operator=(const Serializer& other);
+    ~Serializer();
+};
+
+#endif
