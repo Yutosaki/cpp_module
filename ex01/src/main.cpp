891318da1c5e12ad73e0107f19583f288cd3cce9
Yutosaki <sasayu8190@gmail.com>
2026-01-30 15:30:02 +0000

    feat: add test and functions

---
+++ ex01/src/main.cpp
@@ -0,0 +1,28 @@
+#include <stdint.h>
+
+#include <iostream>
+
+#include "../include/Data.h"
+#include "../include/Serializer.h"
+
+int main() {
+    Data* data = new Data();
+    data->name = "test";
+    data->value = 42;
+
+    std::cout << "Address: " << data << "  Name: " << data->name
+              << "  Value: " << data->value << '\n';
+
+    uintptr_t serialized = Serializer::serialize(data);
+    std::cout << "Serialized: " << serialized << '\n';
+
+    Data* deserialized = Serializer::deserialize(serialized);
+    std::cout << "Address: " << deserialized << "  Name: " << deserialized->name
+              << "  Value: " << deserialized->value << '\n';
+
+    std::cout << "same pointer: " << (data == deserialized ? "true" : "false")
+              << '\n';
+
+    delete data;
+    return 0;
+}
